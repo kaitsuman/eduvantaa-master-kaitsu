c6140447492db95dd8d908de104c1a9c6707abd6
kaitsuman <kaitsuman@users.noreply.github.com>
2019-11-07 12:42:06 +0200

    d

--- aivot.js
+++ aivot.js
@@ -11,19 +11,19 @@ function laatikkoFunktio(boxNumber){
         document.getElementById("laatikko2").innerHTML =
         "<table style='width:100%'>" +
             "<tr>" +
-                "<th>Firstname</th>" +
-                "<th>Lastname</th>" +
-                "<th>Age</th>" +
+                "<th>21</th>" +
+                "<th>Lafase</th>" +
+                "<th>42fgds</th>" +
             "</tr>" +
             "<tr>" +
-                "<td>Jill</td>" +
-                "<td>Smith</td>" +
-                "<td>50</td>" +
+                "<td>dosl</td>" +
+                "<td>Smdsafh</td>" +
+                "<td>5fa0</td>" +
             "</tr>" +
             "<tr>" +
-                "<td>Eve</td>" +
-                "<td>Jackson</td>" +
-                "<td>94</td>" +
+                "<td>Evdsagfe</td>" +
+                "<td>Jaasdcgdksohfn</td>" +
+                "<td>9dsag4</td>" +
             "</tr>" +
         "</table>";
     }
