a86031dbdc8465b7720c04b623980fd02a71cd60
kaitsuman <kaitsuman@users.noreply.github.com>
2019-11-07 12:29:59 +0200

    Update aivot.js

--- aivot.js
+++ aivot.js
@@ -8,7 +8,24 @@ function laatikkoFunktio(boxNumber){
         document.getElementById("laatikko1").style.lineHeight = "50px";
     }
     else if (boxNumber == 2){
-        // toiminnallisuus puuttuu!
+        document:getElementById("laatikko2").innerHTML =
+        "<table style='width:100%'>" +
+            "<tr>" +
+                "<th>Firstname</th>" +
+                "<th>Lastname</th>" +
+                "<th>Age</th>" +
+            "</tr>" +
+            "<tr>" +
+                "<td>Jill</td>" +
+                "<td>Smith</td>" +
+                "<td>50</td>" +
+            "</tr>" +
+            "<tr>" +
+                "<td>Eve</td>" +
+                "<td>Jackson</td>" +
+                "<td>94</td>" +
+            "</tr>" +
+        "</table>";
     }
     else{
         //toiminnallisuus puuttuu!
